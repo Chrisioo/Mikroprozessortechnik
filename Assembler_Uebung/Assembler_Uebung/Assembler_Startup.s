1034de4657f7a7779ffc2e70e60b9fed50e178ad
Christian Petry <christianpetry5@gmail.com>
2024-12-15 16:30:07 +0100

    Hauptprogramm

--- Assembler_Uebung/Assembler_Uebung/Assembler_Startup.s
+++ Assembler_Uebung/Assembler_Uebung/Assembler_Startup.s
@@ -7,17 +7,17 @@
 ;* Programmrumpf fuer Assembler-Programme mit dem Keil				*
 ;* Entwicklungsprogramm uVision fuer ARM-Mikrocontroller			*
 ;********************************************************************
-;* Aufgabe-Nr.:         	*	               						*
+;* Aufgabe-Nr.:         	* 1	               						*
 ;*              			*						    			*
 ;********************************************************************
-;* Gruppen-Nr.: 			*										*
+;* Gruppen-Nr.: 			* 3. Gruppe Freitag						*
 ;*              			*										*
 ;********************************************************************
-;* Name / Matrikel-Nr.: 	*										*
-;*							*										*
+;* Name / Matrikel-Nr.: 	* Christian Petry / 3847497				*
+;*							* Xudong Zhang /						*
 ;*							*										*
 ;********************************************************************
-;* Abgabedatum:         	*              							*
+;* Abgabedatum:         	* 20.12.2024            				*
 ;*							*										*
 ;********************************************************************
 
@@ -26,7 +26,8 @@
 ;********************************************************************
 				AREA		Daten, DATA, READWRITE
 Datenanfang
-Quadrat			EQU			Datenanfang
+STR_1			DCB			"65535", 0
+STR_2 			SPACE		16
 
 ;********************************************************************
 ;* Programm-Bereich bzw. Programm-Speicher							*
@@ -38,13 +39,21 @@ Reset_Handler	MSR			CPSR_c, #0x10	; User Mode aktivieren
 ;********************************************************************
 ;* Hier das eigene (Haupt-)Programm einfuegen   					*
 ;********************************************************************
+; Hauptprogramm
+; R0 = Adresse STR_1
+;AtouI - Konvertiert ASCII-Zeichen in Integer, X in R0
+; Berechnung - Berechnet Wert Y in R0
+; uItoA - Konvertiert Integer in ASCII-Zeichen & speichert in STR_2, Y in R1
 
-				LDR			R3,=Wert 		; Adresse des Werts laden
-				LDR	   		R1,[R3]			; Wert laden
-				MUL			R2,R1,R1		; Quadrat bilden
-				LDR     	R3,=Quadrat		; Adresse des Quadrats laden
-				STR			R2,[R3]			; Quadrat ablegen
-				BL			Demo_UP
+                ENTRY				   		; Programmstart
+				LDR         R0, =STR_1     	; Schreiben der Adresse von STR_1 in R0
+                BL          AtouI          	; Aufruf von AtouI
+
+                BL          Berechnung     	; Aufruf von Berechnung
+
+                LDR         R1, =STR_2     	; Schreiben der Adresse von STR_2 in R1
+                BL          uItoA          	; Aufruf von uItoA
+
 				
 ;********************************************************************
 ;* Ende des eigenen (Haupt-)Programms                               *
@@ -54,8 +63,7 @@ endlos			B			endlos
 ;********************************************************************
 ;* ab hier Unterprogramme                                           *
 ;********************************************************************
-Demo_UP			MVN    		R4, #0
-				BX   		LR
+
 
 ;********************************************************************
 ;* Konstanten im CODE-Bereich                                       *
