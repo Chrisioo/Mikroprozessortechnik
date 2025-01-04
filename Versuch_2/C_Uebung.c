157a52cac829387f8b827cef6d83dbbd3e331b6a
Christian Petry <christianpetry5@gmail.com>
2025-01-04 14:16:34 +0100

    Folders for each sub-task

---
+++ Versuch_2/C_Uebung.c
@@ -0,0 +1,38 @@
+/********************************************************************/
+/*  Hochschule fuer Technik und Wirtschaft                          */
+/*  Fakultaet fuer Ingenieurwissenschaften                          */
+/*  Labor fuer Eingebettete Systeme                                 */
+/*  Mikroprozessortechnik                                           */
+/********************************************************************/
+/*                                                                  */
+/*  C_Uebung.C:                                                     */
+/*	  Programmrumpf fuer C-Programme mit dem Keil                   */
+/*    Entwicklungsprogramm uVision fuer ARM-Mikrocontroller         */
+/*                                                                  */
+/********************************************************************/
+/*  Aufgaben-Nr.:        * 2                                        */
+/*                       *                                          */
+/********************************************************************/
+/*  Gruppen-Nr.: 	     * 3. Gruppe Freitag 1 + 2 Stunde           */
+/*                       *                                          */
+/********************************************************************/
+/*  Name / Matrikel-Nr.: * Christian Petry / 3847497                */
+/*                       * Xudong Zhang / 5014211                   */
+/*                       *                                          */
+/********************************************************************/
+/* 	Abgabedatum:         *                                          */
+/*                       *                                          */
+/********************************************************************/
+
+#include <LPC21xx.H>		/* LPC21xx Definitionen                     */
+
+int main (void)  
+{
+	/* Initialisierung */
+	
+ 	/* Endlosschleife */	
+ 	while (1)  
+	{
+		;
+	}
+}
