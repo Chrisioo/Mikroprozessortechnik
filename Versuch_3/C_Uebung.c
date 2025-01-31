972aadb8c5469020dc41fd1791d6cea0c36a02cd
Christian Petry <christianpetry5@gmail.com>
2025-01-31 10:43:11 +0100

    Update

--- Versuch_3/C_Uebung.c
+++ Versuch_3/C_Uebung.c
@@ -27,25 +27,24 @@
 #include "LPC21xx.h" /* LPC21xx Definitionen */
 #include "Header.h"
 
-void initUart1(int baudRate, short datenBits, short stopBits, short parity)
-{
+void initUart1(unsigned int baudRate, unsigned short datenBits, unsigned short stopBits, unsigned short parity, unsigned short parityMode) {
 
 	unsigned int divisor;
 
 	/* 1. Port-Pins für TxD und RxD konfigurieren */
-	PINSEL0 |= 0x00050000; // P0.8 = TXD1, P0.9 = RXD1, to aktivieren UART1
-
+	PINSEL0 |= 0x00050000; 	// P0.8 = TXD1, P0.9 = RXD1, um UART1 zu nutzen
+						    
 	/* 2. Anzahl Datenbits, Stop-Bits und Parität in UxLCR einstellen dabei DLAB-Bit setzen */
 	U1LCR = 0x80;						/* DLAB = 1, um auf den Divisor zuzugreifen */
 	U1LCR |= ((datenBits - 5) & 0x03) | /* 5..8 Datenbits */
 			 ((stopBits == 2) << 2) |	/* 1 oder 2 Stopbits */
-			 ((parity != 0) << 3) |		/* Paritaet: 0=keine, 1=gerade aktiviert */
-			 ((parity == 1) << 4);		/* Paritaetsmodus: 00 = ungerade, 01 = gerade */
+			 ((parity != 0) << 3) |	/* Paritaetsmodus: 0 = kein Paritaetsbit, 1 = Paritaetsbit */
+			 (parityMode<< 4);		/* Paritaet: 0 = ungerade, 1 = gerade */
 
 	/* 3. Frequenzteiler für Baudrate berechnen. Low-Byte in UxDLL. High-Byte UxDLM. */
 	divisor = PERIPHERIE_CLOCK / (16 * baudRate);
-	U1DLM = (divisor >> 8) & 0xFF;
-	U1DLL = divisor & 0xFF;
+	U1DLM = divisor / 256;
+	U1DLL = divisor % 256;
 
 	/* 4. DLAB-Bit in UxLCR löschen */
 	U1LCR &= ~0x80; // DLAB = 0
@@ -58,8 +57,7 @@ void initUart1(int baudRate, short datenBits, short stopBits, short parity)
 }
 
 // Sendet ein Zeichen über UART1
-void UART1_sendChar(char c)
-{
+void UART1_sendChar(char c) {
 	/* Warten, bis Sendepuffer leer ist */
 	while (!(U1LSR & 0x20))
 		; // U1LSR: Line Status Register, 0x20: THRE (Transmitter Holding Register Empty) bit,
@@ -73,16 +71,14 @@ void UART1_sendChar(char c)
 void UART1_sendString(char *s)
 {
 
-	while (*s) // solange das aktuelle Zeichen nicht das Nullzeichen ist
-	{
+	while (*s) { // solange das aktuelle Zeichen nicht das Nullzeichen ist
 		UART1_sendChar(*s);	// sende das aktuelle Zeichen
 		s++;				// gehe zum nächsten Zeichen
 	}
 }
 
 // Empfaengt ein Zeichen über UART1
-char UART1_receiveChar()
-{
+char UART1_receiveChar() {
 	/* Warten, bis Empfangspuffer gefüllt ist */
 	while (!(U1LSR & 0x01))
 		;
@@ -91,53 +87,47 @@ char UART1_receiveChar()
 }
 
 // Sendet einen Hex-Dump über UART1
-void UART1_sendHexDump(unsigned char *address)
-{
+void UART1_sendHexDump(unsigned char *address) {
 	int i;
 	char hexDigits[] = "0123456789ABCDEF"; // used as a lookup table for converting a nibble to a hex digit
 
 	/* send address */
-	for (i = 28; i >= 0; i -= 4)	
-	{
-		UART1_sendChar(hexDigits[((unsigned int)address >> i) & 0xF]);
+	for (i = 28; i >= 0; i -= 4) { // Loop through the 32-bit address in 4-bit nibbles
+		UART1_sendChar(hexDigits[((unsigned int)address >> i) & 0xF]);	// send the hex digit corresponding to the current nibble
 	}
-	UART1_sendString(": ");
+	UART1_sendString(": "); // send a colon and a space
 
 	/* send 16 bytes of data */
-	for (i = 0; i < 16; i++)
-	{
-		unsigned char c = *address++;
-		UART1_sendChar(hexDigits[(c >> 4) & 0xF]);
-		UART1_sendChar(hexDigits[c & 0xF]);
-		UART1_sendChar(' ');
+	for (i = 0; i < 16; i++) { // Loop through the 16 bytes of data
+		unsigned char c = *address++;				// get the current byte of data
+		UART1_sendChar(hexDigits[(c >> 4) & 0xF]);	// send the high nibble
+		UART1_sendChar(hexDigits[c & 0xF]);			// send the low nibble
+		UART1_sendChar(' ');						// send a space		
 	}
 
-	UART1_sendString("\r\n");
+	UART1_sendString("\r\n");						// send a newline
 }
 
-int main(void)
-{
+int main(void) {
 	char input[9]; // initialize an input buffer with 8 characters and a null terminator
-	int index = 0;
-	int i;
+	short index = 0;	// initialize the index of the input buffer
+	short i;
 	unsigned int address;
-	char hexDigits[] = "0123456789ABCDEF";
+	char hexDigits[] = "0123456789ABCDEF"; // used as a lookup table for converting a nibble to a hex digit
 
 	/* Initialisierung des UART1 */
-	initUart1(BAUDRATE, DATENBITS, STOPBITS, PARITY);
+	initUart1(BAUDRATE, DATENBITS, STOPBITS, PARITY, PARITY_EVEN);
 
-	UART1_sendString("0123456789\r\n");
-	UART1_sendString("Ready to receive address:\r\n");
+	UART1_sendString("0123456789\r\n");		// Test string
+	UART1_sendString("Ready to receive address:\r\n"); // Print a message to indicate that the program is ready to receive an address
 
 	/* Endlosschleife */
-	while (1)
-	{
-		char receivedChar = UART1_receiveChar();
+	while (1) {
+		char receivedChar = UART1_receiveChar();	// receive a character from UART1
 
 		UART1_sendChar(receivedChar); // echo the received character
 
-		if (receivedChar == '\r') // CR (0x0D) stands for the end of input
-		{
+		if (receivedChar == '\r') {  // CR (0x0D) stands for the end of input
 			input[index] = '\0'; // null-terminate the string
 			address = 0;
 
@@ -146,35 +136,27 @@ int main(void)
 			UART1_sendString(input);
 			UART1_sendString("\r\n");
 
-			for (i = 0; i < index; i++)
-			{
+			for (i = 0; i < index; i++) {
 				char c = input[i];
 				address <<= 4;
-				if (c >= '0' && c <= '9')
-				{
+				if (c >= '0' && c <= '9') {
 					address |= (c - '0');
-				}
-				else if (c >= 'A' && c <= 'F')
-				{
+				} else if (c >= 'A' && c <= 'F') {
 					address |= (c - 'A' + 10);
-				}
-				else if (c >= 'a' && c <= 'f')
-				{
+				} else if (c >= 'a' && c <= 'f') {
 					address |= (c - 'a' + 10);
 				}
 			}
 
 			/* Debug: print the converted address */
 			UART1_sendString("Converted address: ");
-			for (i = 28; i >= 0; i -= 4)
-			{
+			for (i = 28; i >= 0; i -= 4) {
 				UART1_sendChar(hexDigits[(address >> i) & 0xF]);
 			}
 			UART1_sendString("\r\n");
 
 			/* check if the address is in the valid range */
-			if (address < 0x40000000 || address > 0x40007FFF)
-			{
+			if (address < ADRESS_MIN || address > ADRESS_MAX) {
 				UART1_sendString("Invalid address range.\r\n");
 				index = 0; // reset index for next input
 				continue;
@@ -182,9 +164,7 @@ int main(void)
 
 			UART1_sendHexDump((unsigned char *)address);
 			index = 0; // reset index for next input
-		}
-		else if (index < 8)
-		{ /* make sure we don't overflow the input buffer */
+		} else if (index < 8) { /* make sure we don't overflow the input buffer */
 			input[index++] = receivedChar;
 		}
 	}
--- Versuch_3/Header.h
+++ Versuch_3/Header.h
@@ -4,7 +4,16 @@
 #define BAUDRATE 19200UL                    // Vorgegebene Baudrate, Anzahl übertragener Bits pro Sekunde
 #define DATENBITS 8UL                       // Vorgegebene Anzahl an Datenbits
 #define STOPBITS 2UL                        // Vorgegebene Anzahl an Stopbits
-#define PARITY 1UL                          // Vorgegebene Paritätsbits
+#define PARITYMODE 0x01UL                   // Vorgegebene Paritätsbits
+#define PARITY 0x00UL                       // Vorgegebene Parität
 #define PERIPHERIE_CLOCK 12500000UL         // Vorgegebene Peripherie-Taktfrequenz, 1/4 der CPU-Frequenz
+
+#define PARITY_ODD 0x01UL                   // Paritätsbit ungerade
+#define PARITY_EVEN 0x00UL                  // Paritätsbit gerade
+#define PARITY_ALWAYS_ONE 0x02UL            // Paritätsbit immer 1
+#define PARITY_ALWAYS_ZERO 0x03UL           // Paritätsbit immer 0
+
+#define ADRESS_MIN 0x40000000UL             // Vorgegebene Mindestadresse
+#define ADRESS_MAX 0x40007FFFUL             // Vorgegebene Maximaladresse
 
 #endif
